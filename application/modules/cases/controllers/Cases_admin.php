2fe9bcd0bb83e58894b6fc5d16e742fb350e9871
Sistiandy <sistiandy@artikulpi.com>
2016-11-22 10:06:25 +0700

    Perbaikan hapus di cases

--- application/modules/cases/controllers/Cases_admin.php
+++ application/modules/cases/controllers/Cases_admin.php
@@ -620,4 +620,36 @@ class Cases_admin extends CI_Controller {
         }
     }
 
+    // Delete to database
+    public function delete($id = NULL) {
+        if ($_POST) {
+            
+            $disposisi = $this->Cases_model->getCasesDisposisi(array('case_id' => $id));
+            $pasal = $this->Cases_model->getHasPasal(array('case_id' => $id));
+            foreach ($disposisi as $row){
+                $this->Cases_model->deleteCasesDisposisi($row['cases_disposisi_id']);
+            }
+            foreach ($pasal as $row){
+                $this->Cases_model->deleteHasPasal($row['cases_has_pasal_id']);
+            }
+            
+            $this->Cases_model->delete($id);
+            // activity log
+            $this->load->model('logs/Logs_model');
+            $this->Logs_model->add(
+                    array(
+                        'log_date' => date('Y-m-d H:i:s'),
+                        'user_id' => $this->session->userdata('uid'),
+                        'log_module' => 'Kasus',
+                        'log_action' => 'Hapus',
+                        'log_info' => 'ID:' . $id . ';Title:' . $this->input->post('delName')
+                    )
+            );
+            $this->session->set_flashdata('success', 'Hapus Kasus berhasil');
+            redirect('admin/cases');
+        } elseif (!$_POST) {
+            redirect('admin/cases');
+        }
+    }
+
 }
